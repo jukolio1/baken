77386fe8775d7c1dbf3f8ae7d53f2c7246163583
david <d63181073@gmail.com>
2024-03-17 23:17:32 -0400

    postgres

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import  express  from "express";
+import {config} from 'dotenv'
+import pg  from 'pg'
+
+config()
 
 const app =  express()
+  const pool =  new pg.Pool({
+connectionString: process.env.DATABASE_URL,
+ssl: false
 
+})
 app.get('/',(req,res) => {
     res.send('Hola Hola')
+})
+
+app.get('/ping', async (req,res) => {
+    const result = await pool.query( 'SELEC NOW()')
+    return res.jason(result.rows[0])
 })
 
 app.listen(3000)
